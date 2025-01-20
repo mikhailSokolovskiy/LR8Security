bfeedde4dcb757243c52f8b580c4f341ce0a055f
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2025-01-20 20:04:57 +0500

    lr8

---
+++ src/main/java/ru/urfu/testsecurity2dbthemeleaf/Lr08SpringApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.testsecurity2dbthemeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lr08SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lr08SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/testsecurity2dbthemeleaf/Lr08SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.testsecurity2dbthemeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lr08SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
